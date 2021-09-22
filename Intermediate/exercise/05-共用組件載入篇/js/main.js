63be53c0dbb745408245f9ca916e90b444c9ad94
Evan <happyday9837@gmail.com>
2021-09-23 00:52:20 +0800

    restful api & regular expression

--- Intermediate/exercise/05-共用組件載入篇/js/main.js
+++ Intermediate/exercise/05-共用組件載入篇/js/main.js
@@ -1,4 +1,19 @@
-window.onload = function(){
+window.onload = function () {
+    var header = document.getElementById("header");
+    var footer = document.getElementById("footer");
 
-    
+    function headerDom() {
+        return axios.get("./components/header.html")
+    };
+    function footerDom() {
+        return axios.get("./components/footer.html")
+    };
+
+    axios.all([headerDom(), footerDom()])
+        .then(axios.spread(function (headDom, footDom) {
+            header.innerHTML = headDom.data;
+            footer.innerHTML = footDom.data;
+
+        }))
+
 }
